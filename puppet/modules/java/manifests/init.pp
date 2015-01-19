cd3ae93bc50b36e237d5030c30ac851fce350da1
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2015-01-19 14:08:03 +0000

    cleaned up java module's init.pp

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -9,55 +9,41 @@ class java(
         path => [ "/usr/bin", "/bin", "/usr/sbin"]
     }
 	
-    file { "/etc/profile.d/java.sh" :
-        content => " export JAVA_HOME=${java_home} \n export PATH=\$PATH:\$JAVA_HOME/bin \n "
-    }
-
-    exec { "set java" :
-        require => Exec["install java"],
-        command => "update-alternatives --set java ${java_home}/bin/java"
-    }
-
-    exec { "set javac" : 
-        require => Exec["install javac"],
-        command => "update-alternatives --set javac ${java_home}/bin/javac"
-    }
-	
-	
-    exec { "install java" :
-        require => Exec["move jdk"],
-        command => "update-alternatives --install /bin/java java ${java_home}/bin/java 1"
-    }
-
-    exec { "install javac" :
-        require => Exec["move jdk"],
-        command => "update-alternatives --install /bin/javac javac ${java_home}/bin/javac 1"
-    }
-
-    exec { "move jdk" :
-        creates => $java_home,
-        require => File["/usr/lib/jvm"],
-        cwd => "/tmp",
-        command => "mv ${java_folder} /usr/lib/jvm/"
-    }
-
-    file { "/usr/lib/jvm" :
-        require => Exec["extract jdk"],
-        owner => vagrant,
-		ensure => directory
-    }
-
-    exec { "extract jdk" :
-        creates => $java_home,
-        cwd => "/tmp",
-        command => "tar xfv ${java_archive}",
-        require => File["/tmp/${java_archive}"]
-    }
-	
     file { "/tmp/${java_archive}" :
         source => "puppet:///modules/java/${java_archive}",
         owner => vagrant,
         mode => 755
+    }
+    ->
+    file { "/usr/lib/jvm" :
+        owner => vagrant,
+		ensure => directory
+    }
+    ->
+    exec { "extract jdk" :
+        creates => $java_home,
+        cwd => "/tmp",
+        command => "tar xfv ${java_archive} -C /usr/lib/jvm/"
+    }
+    ->
+    exec { "install javac" :
+        command => "update-alternatives --install /bin/javac javac ${java_home}/bin/javac 1"
+    }
+    ->
+    exec { "install java" :
+        command => "update-alternatives --install /bin/java java ${java_home}/bin/java 1"
+    }
+    ->
+    exec { "set javac" : 
+        command => "update-alternatives --set javac ${java_home}/bin/javac"
+    }
+    ->
+    exec { "set java" :
+        command => "update-alternatives --set java ${java_home}/bin/java"
+    }
+    ->
+    file { "/etc/profile.d/java.sh" :
+        content => " export JAVA_HOME=${java_home} \n export PATH=\$PATH:\$JAVA_HOME/bin \n "
     }
 
 }
